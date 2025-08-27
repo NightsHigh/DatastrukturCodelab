b4463deda804ce50236b6cc266529e9fe20d970e
eske <eske.larsen@gmail.com>
2025-08-27 13:12:52 +0200

    Changed key to use a single key for the users

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,33 +1,59 @@
 // write cool JS hwere!!
- const userSelect = document.getElementById("userSelect");
-    const nameInput = document.getElementById("nameInput");
-    const ageInput = document.getElementById("ageInput");
-    const colorInput = document.getElementById("colorInput");
-    const saveButton = document.getElementById("saveButton");
+const userSelect = document.getElementById("userSelect");
+const nameInput = document.getElementById("nameInput");
+const ageInput = document.getElementById("ageInput");
+const colorInput = document.getElementById("colorInput");
+const saveButton = document.getElementById("saveButton");
 
-    // Event listener: vis data for valgt bruger
-  function userSelectChange() {
-      const selectedUser = userSelect.value;
-       console.log('change user to: '+selectedUser);
-      // TODO: Hent data fra localStorage og vis i formularen
-      const userData = JSON.parse(localStorage.getItem(selectedUser)) || {name: '', age: '', color: ''};
-      document.body.style.backgroundColor = userData.color || "";
-      nameInput.value = userData.name || "";
-      ageInput.value = userData.age || "";
-    }
+// Initialize empty users if not in localStorage
+if (!localStorage.getItem("users")) {
+  const users = {
+    bruger1: { name: "", age: "", color: "" },
+    bruger2: { name: "", age: "", color: "" },
+    bruger3: { name: "", age: "", color: "" },
+    bruger4: { name: "", age: "", color: "" }
+  };
+  localStorage.setItem("users", JSON.stringify(users));
+}
 
-    // Event listener: gem data fra formularen
-  saveButton.addEventListener("click", () => {
-    const selectedUser = userSelect.value;
-    const userData = {
-      name: nameInput.value,
-      age: ageInput.value,
-      color: colorInput.value
-    };
-      localStorage.setItem(selectedUser, JSON.stringify(userData));
-      document.body.style.backgroundColor = userData.color || "";
-    });
+// Function to save all users
+function saveUsers(users) {
+  localStorage.setItem("users", JSON.stringify(users));
+}
 
-    userSelect.addEventListener("change", userSelectChange);
-    // Vis data for første bruger ved load
-    window.onload = userSelectChange;
+// Event listener: vis data for valgt bruger
+function userSelectChange() {
+  const selectedUser = userSelect.value;
+  console.log('change user to: ' + selectedUser);
+
+  // TODO: Hent data fra localStorage og vis i formularen
+  const users = JSON.parse(localStorage.getItem("users")) || {};
+  const userData = users[selectedUser] || { name: "", age: "", color: "" };
+
+  nameInput.value = userData.name || "";
+  ageInput.value = userData.age || "";
+  colorInput.value = userData.color || "";
+  document.body.style.backgroundColor = userData.color || "";
+}
+
+// Event listener: gem data fra formularen
+saveButton.addEventListener("click", () => {
+  const selectedUser = userSelect.value;
+  const users = getUsers();
+
+  // TODO: Gem data i localStorage for den valgte bruger
+  users[selectedUser] = {
+    name: nameInput.value,
+    age: ageInput.value,
+    color: colorInput.value
+  };
+
+  saveUsers(users);
+  document.body.style.backgroundColor = colorInput.value || "";
+});
+
+// Event listener for dropdown
+userSelect.addEventListener("change", userSelectChange);
+
+// Vis data for første bruger ved load
+window.onload = userSelectChange;
